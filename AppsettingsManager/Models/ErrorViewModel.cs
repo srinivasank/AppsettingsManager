5ef0eb708b6443b7cb358f2d0f098405f0632712
Srinivasan K <srinivasan.k.k@ericsson.com>
2024-07-06 00:53:30 +0530

    Add project files.

---
+++ AppsettingsManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppsettingsManager.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
